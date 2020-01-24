a7b015a305e7810085d96782165fbbe830defac1
genvalen <genvalen@protonmail.com>
2020-01-24 01:24:10 -0500

    Delete If_else.py

--- Python Solutions/If_else.py
+++
@@ -1,8 +0,0 @@
-#Original problem: https://www.hackerrank.com/challenges/py-if-else/problem
-
-N = int(input())
-
-if N%2 != 0 or(N%2==0 and N >= 6 and N <=20):
-    print("Weird")
-else:
-    print("Not Weird")
