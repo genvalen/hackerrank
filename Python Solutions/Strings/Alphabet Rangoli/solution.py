a59e8683ab2c989551bfec415da01e9a30e4e11c
genvalen <genvalen@protonmail.com>
2020-01-30 19:30:47 -0500

    Add solution files

--- Python Solutions/Strings/Alphabet Rangoli/solution.py
+++ Python Solutions/Strings/Alphabet Rangoli/solution.py
@@ -1,1 +1,18 @@
-# Original problem:
+# Original problem: https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+import string
+
+def print_rangoli(n):
+    a = list(string.ascii_lowercase)
+    l = []
+
+    for i in range(n):
+        s = a[i:n] 
+        s = "-".join(s[::-1] + s[1:]).center(4*n-3, "-")
+        l.append(s)
+
+    print(*l[::-1], *l[1:], sep="\n")
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
--- Python Solutions/Strings/Capitalize!/solution.py
+++ Python Solutions/Strings/Capitalize!/solution.py
@@ -1,1 +1,14 @@
-# Original problem:
+# Original problem: https://www.hackerrank.com/challenges/capitalize/problem
+def solve(s):
+    return (" ".join([i.capitalize() if i.isalpha() else i for i in s.split(" ")]))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
