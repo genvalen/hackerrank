e279f8aed7b71fcd75b6185f58c97559b497e9d8
genvalen <genvalen@protonmail.com>
2020-02-24 13:25:22 -0500

    Add solution file

---
+++ Python Solutions/Collections/collections-deque/solution.py
@@ -0,0 +1,11 @@
+# Original problem: https://www.hackerrank.com/challenges/py-collections-deque/problem
+if __name__ == '__main__':
+    from collections import deque
+    
+    d= deque()
+
+    for i in range(int(input())):
+        cmd, *args = input().split()
+        getattr(d, cmd)(*args)
+        
+    print(*d)
