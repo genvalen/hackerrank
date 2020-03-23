edc86f5753c76358f1bfd1bb683ed7dcb158d86b
genvalen <genvalen@protonmail.com>
2020-03-23 01:02:57 -0400

    Add solution file

---
+++ Python Solutions/Math/find-angle-mbc/polar-coordinates/solution.py
@@ -0,0 +1,10 @@
+# Original problem: https://www.hackerrank.com/challenges/polar-coordinates/problem
+if __name__ == '__main__':
+    import cmath
+
+    complex_num = complex(input())
+    r = abs(complex_num)
+    p = cmath.phase(complex_num)
+
+    print(r, p, sep = '\n')
+  
