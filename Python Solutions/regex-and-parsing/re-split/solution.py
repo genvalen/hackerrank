a6cb34cffd2b2ff9697c597d63ba1d3453fa73fd
genvalen <genvalen@protonmail.com>
2020-04-01 15:12:11 -0400

    Add solution file

---
+++ Python Solutions/regex-and-parsing/re-split/solution.py
@@ -0,0 +1,6 @@
+# Original problem: https://www.hackerrank.com/challenges/re-split/problem
+if __name__ == '__main__':
+    import re
+
+    regex_pattern = r'(?<=\d)[.,](?=\d)'
+    print("\n".join(re.split(regex_pattern, input())))
