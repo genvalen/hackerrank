94dd899d16693d1a8b0960116d2b64a42a246a88
genvalen <genvalen@protonmail.com>
2020-06-30 00:17:12 -0400

    Update solution file

--- python-solutions/xml/xml-1-find-the-score/solution.py
+++ python-solutions/xml/xml-1-find-the-score/solution.py
@@ -1,9 +1,17 @@
 # Original problem: https://www.hackerrank.com/challenges/xml-1-find-the-score/problem
-if __name__ == '__main__':
-    def get_attr_number(node):
-    
+import sys
+import xml.etree.ElementTree as etree
+
+def get_attr_number(node):
     score = 0
     for elem in node.iter('*'):
         if elem.attrib:
             score += len(elem.attrib)
     return(score)
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
