e3260dfce2ccc6633baf377969fc0f872c6a6001
genvalen <genvalen@protonmail.com>
2020-07-13 23:26:48 -0400

    Add solution file

---
+++ java-solutions/introduction/java-stdin-and-stdout-2/Solution.java
@@ -0,0 +1,23 @@
+// Original problem: https://www.hackerrank.com/challenges/java-stdin-stdout/problem
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+       
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        String s = ""; //to be replaced by nextLine().
+        
+        while(scan.hasNext() == true){
+            s = scan.nextLine();
+        } 
+
+        scan.close();
+        
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
