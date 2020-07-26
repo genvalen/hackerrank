789d345cc8c15517366d8ce8723ec5bf03838fd3
genvalen <genvalen@protonmail.com>
2020-07-25 22:04:40 -0400

    Add solution file

---
+++ java-solutions/strings/java-strings-introduction/Solution.java
@@ -0,0 +1,35 @@
+// https://www.hackerrank.com/challenges/java-strings-introduction/copy-from/99550701
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+
+        System.out.println(A.length() + B.length());
+
+        
+        int result = A.compareTo(B);
+        if(result > 0){
+           System.out.println("Yes");     
+        }
+        else{
+            System.out.println("No");
+        }
+
+
+        String firstLetterA = A.substring(0,1).toUpperCase();
+        String firstLetterB= B.substring(0,1).toUpperCase();
+
+        String newStringA = firstLetterA + A.substring(1);
+        String newStringB = firstLetterB + B.substring(1);
+
+        System.out.println(newStringA +" "+ newStringB);
+    }
+}
