a0c095c5e1b1b835daabe964fbe5f47ea8bfce93
genvalen <genvalen@protonmail.com>
2020-07-26 23:34:26 -0400

    Add solution file

---
+++ java-solutions/strings/java-substring/Solution.java
@@ -0,0 +1,20 @@
+// Original problem: https://www.hackerrank.com/challenges/java-substring/copy-from/101544262
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        String newString = S.substring(start, end);
+        System.out.println(newString);
+    }
+}
