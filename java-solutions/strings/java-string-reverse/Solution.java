db1ae9614e248711a46a042e76b9e114cbccdc20
genvalen <genvalen@protonmail.com>
2020-08-04 21:40:26 -0400

    Add solution file

---
+++ java-solutions/strings/java-string-reverse/Solution.java
@@ -0,0 +1,24 @@
+// original problem: https://www.hackerrank.com/challenges/java-string-reverse/copy-from/102690443
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A= sc.next();
+        String B = "";
+       for(int i =0; i< A.length(); i++){
+        char letter = A.charAt(i);
+        B = letter + B;
+       }
+        
+    if(A.equals(B)){
+      System.out.println("Yes");  
+    }
+    else{
+        System.out.println("No");}
+    }
+}
